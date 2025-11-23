8ca9980f2bc2eb04c939eb82835e83a0a2924259
Diego201444 <diego.noca@ngsoluciones.com>
2025-11-23 15:47:13 -0300

    Delete public directory

--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Punto de entrada. Por ahora redirigimos a la vista general.
-header('Location: /src/views/general.php');
-exit;
--- public/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Punto de entrada de la aplicación.
-// Carga la vista general que contiene el panel completo.
-require_once __DIR__ . '/../src/views/general.php';
