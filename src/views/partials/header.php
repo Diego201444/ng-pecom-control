5e5c396b71329c3ccd1b9807e1d5067eee561560
Diego201444 <diego.noca@ngsoluciones.com>
2025-11-23 09:37:13 -0800

    feat: add src/views/partials/header.php

---
+++ src/views/partials/header.php
@@ -0,0 +1,18 @@
+<?php
+// Podés cargar config acá si querés
+?>
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>NG – Panel Pecom</title>
+  <link rel="stylesheet" href="/assets/css/app.css">
+</head>
+<body>
+<header>
+  <nav>
+    <a href="/src/views/general.php">General</a> |
+    <a href="/src/views/carga.php">Carga</a> |
+    <a href="/src/views/tanque.php">Tanque</a>
+  </nav>
+</header>
