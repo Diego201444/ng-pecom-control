dee6e3583b335371a9105bff936cf6d8e0a2473a
Diego201444 <diego.noca@ngsoluciones.com>
2025-11-23 09:36:10 -0800

    feat: add src/views/tanque.php

---
+++ src/views/tanque.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/partials/header.php';
+?>
+<main class="container">
+  <h1>Detalle de tanque</h1>
+  <p>Aquí va el mapa del tanque, nivel, temperatura, Vbat, etc.</p>
+</main>
+<?php
+require __DIR__ . '/partials/footer.php';
